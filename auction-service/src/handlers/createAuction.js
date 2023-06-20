50efe504c6d84c9666102a04599b6eb6797a06bb
Matheus <matheusaraujo.dev@gmail.com>
2023-06-20 13:33:00 -0300

    Add: first createAuction function

---
+++ auction-service/src/handlers/createAuction.js
@@ -0,0 +1,21 @@
+async function createAuction(event, context) {
+
+  const body = JSON.parse(event.body)
+  const { title } = body;
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString(),
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = createAuction;
+
+
